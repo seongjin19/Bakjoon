79de6ea4dec82f4c048567618f3c5bf747885cf9
seongjin19 <48229733+seongjin19@users.noreply.github.com>
2022-06-03 17:57:30 +0900

    Add files via upload

---
+++ 14681.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	int x, y;
+	cin >> x;
+	cin >> y;
+
+	if (x > 0 && y > 0)
+	{
+		cout << 1;
+	}
+	else if (x < 0 && y > 0)
+	{
+		cout << 2;
+	}
+	else if (x < 0 && y < 0)
+	{
+		cout << 3;
+	}
+	else if (x > 0 && y < 0)
+	{
+		cout << 4;
+	}
+	
+	return 0;
+}
